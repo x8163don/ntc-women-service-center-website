c5a22e9a5603602d83f2932acf0993f7b860cf1b
Bevis <bevis.jhang@hy-tech.com.tw>
2020-03-01 16:20:36 +0800

    add videos

---
+++ src/modules/center/components/CenterVideos.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div :key="video.url" v-for="video in videos.videos">
+      <h3 class="gray">{{ video.title }}</h3>
+      <iframe
+        :src="video.url"
+        frameborder="0"
+        allow="accelerometer; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+        class="mb5 w-100 h5"
+      ></iframe>
+    </div>
+  </div>
+</template>
+
+<script>
+import Videos from "../data/videos.json"
+
+export default {
+  data() {
+    return {
+      videos: Videos
+    }
+  }
+}
+</script>
+
+<style></style>
--- src/modules/center/views/Center.vue
+++ src/modules/center/views/Center.vue
@@ -1,36 +1,43 @@
 <template>
-  <div>
+  <div class="flex pa4">
     <SideMenu
       :title="title"
       :menuItems="menuItems"
       :defaultSelectedItem="menuItems[0].component"
+      class="w-25"
     ></SideMenu>
 
     <!-- Content -->
-    <div></div>
+    <div class="w-80">
+      <keep-alive>
+        <component v-bind:is="electedItem" class="w-100"></component>
+      </keep-alive>
+    </div>
   </div>
 </template>
 
 <script>
-import SideMenu from "../../common/components/SideMenu";
+import SideMenu from "../../common/components/SideMenu"
+import CenterVideos from "../components/CenterVideos"
 export default {
-  components: { SideMenu },
+  components: { SideMenu, CenterVideos },
   data() {
     return {
       title: "中心出版",
+      electedItem: "CenterVideos",
       menuItems: [
         {
           name: "影音專區",
-          component: ""
+          component: "CenterVideos"
         },
         {
           name: "文宣品/刊物",
           component: ""
         }
       ]
-    };
+    }
   }
-};
+}
 </script>
 
 <style></style>
