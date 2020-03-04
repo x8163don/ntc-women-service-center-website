7fdea61c1655a78754c6d742697f19b990d5ad43
Bevis <bevis.jhang@hy-tech.com.tw>
2020-03-04 21:43:28 +0800

    Add CNAME

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,18 @@
+const CopyPlugin = require("copy-webpack-plugin")
+
 module.exports = {
   publicPath: process.env.VUE_APP_BUILD_PATH,
-  productionSourceMap: false
-};
+  productionSourceMap: false,
+
+  configureWebpack: config => {
+    const plugins = []
+
+    plugins.push(getCopyPlugin())
+
+    config.plugins = [...config.plugins, ...plugins]
+  }
+}
+
+function getCopyPlugin() {
+  return new CopyPlugin([{ from: "CNAME", to: "" }])
+}
