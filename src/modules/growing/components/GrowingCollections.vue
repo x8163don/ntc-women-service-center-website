83b63413b50f71561802c600c09e2e283b8f5c6b
Bevis <bevis.jhang@linecorp.com>
2020-03-06 23:10:00 +0800

    remove img

--- src/modules/growing/components/GrowingCollections.vue
+++ src/modules/growing/components/GrowingCollections.vue
@@ -1,21 +1,17 @@
 <template>
   <div class="">
     <h3>享受．閱讀 / 典藏閱讀</h3>
-    <div class="flex ">
-      <img src="" class="w5 h5" />
-
-      <a
-        class="ml4"
-        href="https://drive.google.com/uc?id=1zuzmOXHByueviKMmgmsYUzQRs_bz7nLN&export=download"
-      >
-        圖書清單
-      </a>
-    </div>
+    <a
+      class="ml4"
+      href="https://drive.google.com/uc?id=1zuzmOXHByueviKMmgmsYUzQRs_bz7nLN&export=download"
+    >
+      圖書清單
+    </a>
   </div>
 </template>
 
 <script>
-export default {}
+export default {};
 </script>
 
 <style></style>
