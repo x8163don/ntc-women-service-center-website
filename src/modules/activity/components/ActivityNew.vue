79f0b55b99d9e75df16c0c6c3b3cd88ed4fef3f3
Bevis <bevis.jhang@linecorp.com>
2020-03-07 23:51:26 +0800

    Add Modal

--- src/modules/activity/components/ActivityNew.vue
+++ src/modules/activity/components/ActivityNew.vue
@@ -3,22 +3,42 @@
     <div
       v-for="activity in activities"
       :key="activity.startDate + activity.imgUrl"
+      @click="openActivity(activity)"
     >
       <div>{{ activity.startDate }} - {{ activity.endDate }}</div>
       <img :src="activity.imgUrl" />
     </div>
+
+    <Modal :show="isModalShow" @close="isModalShow = false">
+      <div class="pa3" v-if="activityInfo">
+        <img class="w-100 h-100" :src="activityInfo.imgUrl" />
+      </div>
+    </Modal>
   </div>
 </template>
 
 <script>
 import Activities from "../data/activities.json";
+import Modal from "../../common/components/Modal.vue";
+
 export default {
+  components: {
+    Modal
+  },
   data() {
     return {
+      isModalShow: false,
+      activityInfo: null,
       activities: Activities.filter(
         item => new Date(item.startDate).getYear() === new Date().getYear()
       )
     };
+  },
+  methods: {
+    openActivity(activity) {
+      this.activityInfo = activity;
+      this.isModalShow = true;
+    }
   }
 };
 </script>
