938a1f3af89d2ab45e20693c91469b4cb8ce1f59
Bevis <bevis.jhang@linecorp.com>
2020-03-15 10:44:55 +0800

    update files

--- src/modules/center/components/CenterFiles.vue
+++ src/modules/center/components/CenterFiles.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
-    <div v-for="fileInfo in files" :key="fileInfo.category">
-      <h3>{{ fileInfo.category }}</h3>
-      <div v-for="file in fileInfo.files" :key="file.fileUrl">
+    <div v-for="fileInfo in files" :key="fileInfo.category" class="mb5">
+      <h3 class="gray">{{ fileInfo.category }}</h3>
+      <div v-for="file in fileInfo.files" :key="file.fileUrl" class="pl3">
         <a :href="file.fileurl">
           <p>{{ file.filename }}</p>
         </a>
