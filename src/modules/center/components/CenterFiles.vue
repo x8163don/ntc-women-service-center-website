22c0bcae647f3f1dcfe261d41a22dc4eef3dcabc
Bevis <bevis.jhang@linecorp.com>
2020-03-15 10:31:02 +0800

    add files

---
+++ src/modules/center/components/CenterFiles.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div v-for="fileInfo in files" :key="fileInfo.category">
+      <h3>{{ fileInfo.category }}</h3>
+      <div v-for="file in fileInfo.files" :key="file.fileUrl">
+        <a :href="file.fileurl">
+          <p>{{ file.filename }}</p>
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Files from "../data/files.json";
+
+export default {
+  data() {
+    return {
+      files: Files
+    };
+  }
+};
+</script>
+
+<style></style>
--- src/modules/center/views/Center.vue
+++ src/modules/center/views/Center.vue
@@ -3,28 +3,31 @@
     <SideMenu
       :title="title"
       :menuItems="menuItems"
-      :defaultSelectedItem="menuItems[0].component"
+      :defaultSelectedItem="selectedItem"
+      @selectMenuItem="selectMenuItem"
       class="w-20"
     ></SideMenu>
 
     <!-- Content -->
     <div class="w-80">
       <keep-alive>
-        <component v-bind:is="electedItem" class="w-100"></component>
+        <component v-bind:is="selectedItem" class="w-100"></component>
       </keep-alive>
     </div>
   </div>
 </template>
 
 <script>
-import SideMenu from "../../common/components/SideMenu"
-import CenterVideos from "../components/CenterVideos"
+import SideMenu from "../../common/components/SideMenu";
+import CenterVideos from "../components/CenterVideos";
+import CenterFiles from "../components/CenterFiles";
+
 export default {
-  components: { SideMenu, CenterVideos },
+  components: { SideMenu, CenterVideos, CenterFiles },
   data() {
     return {
       title: "中心出版",
-      electedItem: "CenterVideos",
+      selectedItem: "CenterVideos",
       menuItems: [
         {
           name: "影音專區",
@@ -32,12 +35,17 @@ export default {
         },
         {
           name: "文宣品/刊物",
-          component: ""
+          component: "CenterFiles"
         }
       ]
+    };
+  },
+  methods: {
+    selectMenuItem(selectedItem) {
+      this.selectedItem = selectedItem.component;
     }
   }
-}
+};
 </script>
 
 <style></style>
