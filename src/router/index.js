568cbc316e501530a5c3ea3e58f6c855cc3cf4cf
Bevis <bevis.jhang@linecorp.com>
2020-03-16 11:43:07 +0800

    add comment

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 
+// import Common Components
 const NavContentLayout = () =>
   import("../modules/common/components/NavContentLayout");
 const DataPreview = () =>
