f7dae50ce86dad0788d3387faddf0724b6c8b810
Bevis <bevis.jhang@hy-tech.com.tw>
2021-05-29 19:34:10 +0800

    update header

--- src/modules/common/components/Header.vue
+++ src/modules/common/components/Header.vue
@@ -1,35 +1,26 @@
 <template>
-  <div class="br2 br--top bg-white w-100">
-    <!-- Logo -->
-    <div class="pl2">
-      <a href="/"><img class="h3 db dtc-ns v-mid tl" :src="logoImage"/></a>
-    </div>
+  <nav class="z-9999 fixed flex bb b--white-10 nav">
+    <a class="link white-70 hover-white no-underline pa3" href="">
+      <img src="../../../assets/logo.png" class="logo" alt="logo" />
+    </a>
 
-    <!-- Menu -->
-    <div class="flex items-center bg-blue">
+    <div class="pr3 pl3 pt2 pb2 flex justify-center items-center">
       <template v-for="(link, index) in links">
         <router-link
           :key="index"
-          class="flex-auto pa3 tc no-underline default-text-color pointer"
+          class="f5 link dib tc white dim mr3 mr4-ns lh-title"
           :to="link.to"
           >{{ link.showText }}</router-link
         >
-        <div
-          :key="'b' + index"
-          v-if="index !== links.length - 1"
-          class="br bw1 h1 dib"
-        ></div>
       </template>
     </div>
-  </div>
+  </nav>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      logoImage:
-        "https://lh3.googleusercontent.com/laewn4vxesa8lrD5YnkBJmWtPUJZ-xUC0WHcqQ_RpaK79btXjWWAm2Orv_LYCC3Bx4qSN_SAZtpg9BJN9RNurGdSAHx7HbI5DMchXx2IUo_YtjkZOqtdLFABsAO_ggsKzHSCuJXPaggIKJW2-3_Zhbmau-OdmDQz0_60nGq1L4kp6CXfVg86y78TjXX0NH4TfRWsILlo-RVkzsqimuubgnb6XzbCUy5xPVUUyPNyhKG-7rdAY0vdzQsAlOSB5FYfnKnGiZuBuPu4eQfBNMy1gp5133NGtqaVSz7CI_VlkltVuW0qdZCnOrhTJUFTnGmU1o6dc0Z0NuddwHwXVua_Qtu6RGQxXMkgU4KdhDj3X1Mcer8ePkV9wqOEBCZ_3qQTDglNZAQ6KfUwKMXrB1PDCGDWivG2UcLZzOfpr5Nu06VMLSV7eB0Xbt14w9WD7j2cTXRLetf42pin3A4SE88v9nVdjz9YTLRH-dpvd_G_mHH12Wv7eHit1GRCFCtjcWZ-G2kKjrOm-b3QoyrCHIASUf89Dtvja6ZhjtoJPToFpMoq2owhFI3Nx_P6iDOUJSXcbKbW0p0Lg4Ijms-jjYJM9R_z0BFo0K-iVugaA4hcd9Nb9nxgZREy27bqG2GkwOIZyamhBJMbqESlTFfwQV_sOA0xvPTjarrDmM7jBUPAx4a5S3Oauit4Dh5W14Vrsqhz9tH1pmKTvid4EIyutqzS0ACZDgrCUi_Vv1EU5froB-3bve0=w920-h375-no",
       links: [
         { to: "/about", showText: "關於我們" },
         { to: "/news", showText: "最新消息" },
@@ -38,21 +29,23 @@ export default {
         { to: "/power", showText: "女力加油站" },
         { to: "/videos", showText: "性平影音" },
         { to: "/angel", showText: "微光天使" },
-        { to: "/exhibitions", showText: "線上展覽" }
-      ]
-    };
-  }
-};
+        { to: "/exhibitions", showText: "線上展覽" },
+      ],
+    }
+  },
+}
 </script>
 
 <style scoped>
 .nav {
-  padding: 30px;
-  background-color: #f68657;
+  width: 100%;
+  background-color: #f3b7bf;
 }
 
-.menu-item {
-  flex: 1;
+.logo {
+  margin-left: 60px;
+  width: 32px;
+  height: 32px;
 }
 
 .router-link-exact-active {
