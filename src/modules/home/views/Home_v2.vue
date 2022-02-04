8d0c219182acb4bd7337dd8b0dfcd625545338e9
Bevis <x8163don@gmail.com>
2022-02-04 16:21:13 +0800

    feat: add recently activities

--- src/modules/home/views/Home_v2.vue
+++ src/modules/home/views/Home_v2.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
+  <div class="home">
     <Header></Header>
+    <div class="banner"><img src="../../../assets/home/banner.jpg" /></div>
+    <RecentlyActivities></RecentlyActivities>
     <Footer></Footer>
   </div>
 </template>
@@ -8,11 +10,23 @@
 <script>
 import Header from "../../common/components/HeaderV2";
 import Footer from "../../common/components/FooterV2";
+import RecentlyActivities from "../components/RecentlyActivities";
 
 export default {
   components: {
     Header,
+    RecentlyActivities,
     Footer
+  },
+  data() {
+    return {};
   }
 };
 </script>
+
+<style scoped>
+.home {
+  width: 1200px;
+  margin: 0 auto;
+}
+</style>
