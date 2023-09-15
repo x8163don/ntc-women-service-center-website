ada23237ca31d064fd02c87fb0d0a2b30d30822d
Bevis <x8163don@gmail.com>
2023-09-15 16:49:40 +0800

    feat: add google calendar

--- src/modules/about/views/Booking.vue
+++ src/modules/about/views/Booking.vue
@@ -3,13 +3,19 @@
   <div class="main_container">
     <div class="content_container">
       <h1>新北市婦女服務中心場地使用管理要點</h1>
+
+      <div class="img_container">
+        <img class="place_img" src="../../../assets/about/booking/place1.jpg"/>
+        <img class="place_img" src="../../../assets/about/booking/place2.jpg"/>
+      </div>
+
       <p>
         <img
-          class="icon_check"
-          src="../../../assets/about/booking/Icon open-circle-check.png"
+            class="icon_check"
+            src="../../../assets/about/booking/Icon open-circle-check.png"
         />
         第一點
-        <br />
+        <br/>
         新北市婦女服務中心（以下簡稱本中心）以舉辦婦女活動，打造婦女培力、交流、展演等多功能場地為原則，以提供婦女服務相關社團優先使用，並開放給非營利團體多元使用。
         本中心為能發揮空間多元化的服務及管理功能，特訂定本要點。
         本中心場地酌收之水電費、清潔費等必要費用，將挹注於本場地之使用，回饋舉辦婦女活動、場地維護及培力婦女公民參與及社會關懷等，以達創造婦女友善空間的目標。
@@ -17,176 +23,182 @@
 
       <p>
         <img
-          class="icon_check"
-          src="../../../assets/about/booking/Icon open-circle-check.png"
+            class="icon_check"
+            src="../../../assets/about/booking/Icon open-circle-check.png"
         />
         第二點
-        <br />
+        <br/>
         本中心場地管理單位為社團法人新北市志願服務協會。
       </p>
       <p>
         <img
-          class="icon_check"
-          src="../../../assets/about/booking/Icon open-circle-check.png"
+            class="icon_check"
+            src="../../../assets/about/booking/Icon open-circle-check.png"
         />
         第三點
-        <br />
+        <br/>
         一、本中心場地空間使用範圍如下：
-        <br />
-        （一）內部空間：閱覽室、展覽廳、會議室。<br />
-        （二）外部空間：親子小屋、漂書區、ㄇ字區、空廊。<br />
-        （三）其他經本中心認定之場地。<br />
-        二、申請借用時間及時段： <br />
-        （一）本中心開放時間為：週一至週五8：30－17：30；週六、週日暨國定假日休館。<br />
-        （二）會議室借用時段區分如下：<br />
-        1.上午時段：上午9時至中午12時<br />
-        2.下午時段：下午14時至下午17時<br />
-        3.全日: 上午9時至下午17時<br />
-        4.申請場地佈置及恢復之時間，請依本中心開放時間填寫申請，並於申請時一併提出。<br />
+        <br/>
+        （一）內部空間：閱覽室、展覽廳、會議室。<br/>
+        （二）外部空間：親子小屋、漂書區、ㄇ字區、空廊。<br/>
+        （三）其他經本中心認定之場地。<br/>
+        二、申請借用時間及時段： <br/>
+        （一）本中心開放時間為：週一至週五8：30－17：30；週六、週日暨國定假日休館。<br/>
+        （二）會議室借用時段區分如下：<br/>
+        1.上午時段：上午9時至中午12時<br/>
+        2.下午時段：下午14時至下午17時<br/>
+        3.全日: 上午9時至下午17時<br/>
+        4.申請場地佈置及恢復之時間，請依本中心開放時間填寫申請，並於申請時一併提出。<br/>
       </p>
       <p>
         <img
-          class="icon_check"
-          src="../../../assets/about/booking/Icon open-circle-check.png"
+            class="icon_check"
+            src="../../../assets/about/booking/Icon open-circle-check.png"
         />
-        第四點 借用單位規範及說明： <br />
-        申請本中心會議室空間供使用，應依新北巿婦女服務中心收費標準收取費用，並向本中心申請，以提供下列使用為限：<br />
-        （一）新北巿政府及所屬機關舉辦活動。<br />
-        （二）前款以外之政府機關、經立案之公益團體或民間團體舉辦之公益、教育或藝文活動，並以辦理婦女福利、婦女權益、性別事務等相關業務使用為主。<br />
+        第四點 借用單位規範及說明： <br/>
+        申請本中心會議室空間供使用，應依新北巿婦女服務中心收費標準收取費用，並向本中心申請，以提供下列使用為限：<br/>
+        （一）新北巿政府及所屬機關舉辦活動。<br/>
+        （二）前款以外之政府機關、經立案之公益團體或民間團體舉辦之公益、教育或藝文活動，並以辦理婦女福利、婦女權益、性別事務等相關業務使用為主。<br/>
       </p>
       <p>
         <img
-          class="icon_check"
-          src="../../../assets/about/booking/Icon open-circle-check.png"
+            class="icon_check"
+            src="../../../assets/about/booking/Icon open-circle-check.png"
         />
-        第五點<br />
-        申請使用本中心會議室空間應依下列申請程序辦理： <br />
+        第五點<br/>
+        申請使用本中心會議室空間應依下列申請程序辦理： <br/>
         （一）借用單位應於活動兩週前填妥申請表(如附件)並檢附活動計畫摘要(含目的、主辦單位、時間、地點、參加對象及人數、內容），可至現場填寫申請、傳真或E-mail方式，將申請表送至本中心，經本中心審核後，始完成借用手續。
-        <br />
+        <br/>
         （二）使用優先順序，依申請核准時間之先後，同時提出申請者，以單次活動、時間較短者為優先。
-        <br />
+        <br/>
         （三)經審查核准者，應於使用當天繳交全額保證金及身分證件，租借一個時段為新台幣500元，保證金會於活動結束完，經工作同仁確認場地已復原並且未被任意破壞後，將會全額退還。
-        <br />
+        <br/>
         （四）預訂租用場地之後，如中途放棄使用，應於預定使用日前一週通知本中心，且場地應交由本中心另行安排，不得私自轉讓。
-        <br />
-        (五)開放申請時間為使用日期前三個月。 <br />
+        <br/>
+        (五)開放申請時間為使用日期前三個月。 <br/>
       </p>
 
       <p>
         <img
-          class="icon_check"
-          src="../../../assets/about/booking/Icon open-circle-check.png"
+            class="icon_check"
+            src="../../../assets/about/booking/Icon open-circle-check.png"
         />
-        第六點<br />
-        申請借用場地單位若有下列情形之一者，其申請使用不予核准，已核准者，應立即停止其使用：<br />
-        （一）違反法令規定者。<br />
-        （二）違反公序良俗者。<br />
-        （三）有安全顧慮者。<br />
-        （四）有營業行為者。<br />
-        （五）蓄意破壞公物者。<br />
-        （六）活動影響週邊鄰居安寧，經勸導不改善者。<br />
-        （七）曾借用場地，不遵守管理規定，登記有案，未滿一年者。<br />
-        （八）與原申請內容不符，或將場地私自轉讓他人使用者。<br />
+        第六點<br/>
+        申請借用場地單位若有下列情形之一者，其申請使用不予核准，已核准者，應立即停止其使用：<br/>
+        （一）違反法令規定者。<br/>
+        （二）違反公序良俗者。<br/>
+        （三）有安全顧慮者。<br/>
+        （四）有營業行為者。<br/>
+        （五）蓄意破壞公物者。<br/>
+        （六）活動影響週邊鄰居安寧，經勸導不改善者。<br/>
+        （七）曾借用場地，不遵守管理規定，登記有案，未滿一年者。<br/>
+        （八）與原申請內容不符，或將場地私自轉讓他人使用者。<br/>
       </p>
       <p>
         <img
-          class="icon_check"
-          src="../../../assets/about/booking/Icon open-circle-check.png"
+            class="icon_check"
+            src="../../../assets/about/booking/Icon open-circle-check.png"
         />
-        第七點 <br />
-        本中心會議室使用規定如下：<br />
-        （一）借用會場請自行維持場地內外秩序，如有餐飲請提前告知，並務必將用餐後之廚餘及垃圾自行帶走處理。<br />
-        （二）本中心因辦理活動或臨時需要，必須使用會議室空間時，於一周前通知已登記借用單位收為自用，借用單位不得異議。<br />
-        因前項事宜或遇不可抗力之事致不能使用會議室空間時，登記借用單位得申請延期或由本中心無息退還場地使用費。<br />
-        （三）借用單位辦理活動所需相關用品(如免洗杯/筷、環保餐具、托盤、垃圾袋等)，請借用單位自行準備；如需借用本中心之視聽設備，請於填寫申請表時事先說明，並於活動前確認設備操作方法。(※※請自備：筆記型電腦、腰掛隨身式麥克風及簡報筆)。<br />
-        （四）本中心內各門窗及牆壁均不得使用圖釘、釘槍、漿糊、膠水及雙面膠帶等物品張貼海報、公告或任何文件。活動海報須依規定張貼於指定場所，並於活動後立即清除。（如需張貼海報，請使用專用黏土，或向本中心借用海報架(至多1個)，並提前告知本中心工作人員以便協調）。<br />
-        （五）舉辦活動，請勿超出借用場地範圍，未經同意不得任意佔用公共走道或大廳，或將不用之桌椅堆放於場地之外。<br />
-        （六）若於活動進行中有相關物品需寄送至本中心場地，請廠商直接聯繫申請單位之聯絡人，如需本中心協助代收簽領，請提前告知本中心同仁，以事先安排人力提供協助。<br />
-        （七）使用場地時間以申請表上之借用時間為限，如因流程安排需延長借用時間，請於提出申請時告知並確認後續場地使用狀況，以維持本中心例行活動之運作及市民之權益。<br />
-        （八）活動結束後，使用之場地須恢復原狀，關閉電源及門窗，並清理好場地，垃圾須分類打包自行處理清除丟棄，花圈、花籃及其他非屬本中心之物品，請應於當日清除及運離，離開時應保持場地的整齊清潔並通知本中心場地負責人員確認場地回復狀態。<br />
+        第七點 <br/>
+        本中心會議室使用規定如下：<br/>
+        （一）借用會場請自行維持場地內外秩序，如有餐飲請提前告知，並務必將用餐後之廚餘及垃圾自行帶走處理。<br/>
+        （二）本中心因辦理活動或臨時需要，必須使用會議室空間時，於一周前通知已登記借用單位收為自用，借用單位不得異議。<br/>
+        因前項事宜或遇不可抗力之事致不能使用會議室空間時，登記借用單位得申請延期或由本中心無息退還場地使用費。<br/>
+        （三）借用單位辦理活動所需相關用品(如免洗杯/筷、環保餐具、托盤、垃圾袋等)，請借用單位自行準備；如需借用本中心之視聽設備，請於填寫申請表時事先說明，並於活動前確認設備操作方法。(※※請自備：筆記型電腦、腰掛隨身式麥克風及簡報筆)。<br/>
+        （四）本中心內各門窗及牆壁均不得使用圖釘、釘槍、漿糊、膠水及雙面膠帶等物品張貼海報、公告或任何文件。活動海報須依規定張貼於指定場所，並於活動後立即清除。（如需張貼海報，請使用專用黏土，或向本中心借用海報架(至多1個)，並提前告知本中心工作人員以便協調）。<br/>
+        （五）舉辦活動，請勿超出借用場地範圍，未經同意不得任意佔用公共走道或大廳，或將不用之桌椅堆放於場地之外。<br/>
+        （六）若於活動進行中有相關物品需寄送至本中心場地，請廠商直接聯繫申請單位之聯絡人，如需本中心協助代收簽領，請提前告知本中心同仁，以事先安排人力提供協助。<br/>
+        （七）使用場地時間以申請表上之借用時間為限，如因流程安排需延長借用時間，請於提出申請時告知並確認後續場地使用狀況，以維持本中心例行活動之運作及市民之權益。<br/>
+        （八）活動結束後，使用之場地須恢復原狀，關閉電源及門窗，並清理好場地，垃圾須分類打包自行處理清除丟棄，花圈、花籃及其他非屬本中心之物品，請應於當日清除及運離，離開時應保持場地的整齊清潔並通知本中心場地負責人員確認場地回復狀態。<br/>
       </p>
       <p>
         <img
-          class="icon_check"
-          src="../../../assets/about/booking/Icon open-circle-check.png"
+            class="icon_check"
+            src="../../../assets/about/booking/Icon open-circle-check.png"
         />
-        第八點 <br />
-        本中心對外開放之各場域使用規定如下：<br />
-        （一）禁止攜帶或使用違禁品、危險物品、私接電源，如：延長線、電腦或手機充電線、加裝燈光、照明或其他電器。<br />
-        （二）貴重物品或私有書籍、電子設備、書包及手提袋，應自行保管，如有遺失或毀損，本館不負賠償之責。<br />
-        （三）本中心場地禁止吸菸以及嚼食口香糖、檳榔、飲食、喧譁影響他人、躺臥（含將腳置於桌上）、衣衫不整（含脫鞋、襪）等不雅等行為。<br />
-        （四）為免驚擾他人，中心場館禁帶寵物入內（導盲犬除外）。<br />
+        第八點 <br/>
+        本中心對外開放之各場域使用規定如下：<br/>
+        （一）禁止攜帶或使用違禁品、危險物品、私接電源，如：延長線、電腦或手機充電線、加裝燈光、照明或其他電器。<br/>
+        （二）貴重物品或私有書籍、電子設備、書包及手提袋，應自行保管，如有遺失或毀損，本館不負賠償之責。<br/>
+        （三）本中心場地禁止吸菸以及嚼食口香糖、檳榔、飲食、喧譁影響他人、躺臥（含將腳置於桌上）、衣衫不整（含脫鞋、襪）等不雅等行為。<br/>
+        （四）為免驚擾他人，中心場館禁帶寵物入內（導盲犬除外）。<br/>
       </p>
 
       <p>
         <img
-          class="icon_check"
-          src="../../../assets/about/booking/Icon open-circle-check.png"
+            class="icon_check"
+            src="../../../assets/about/booking/Icon open-circle-check.png"
         />
-        第九點 <br />
-        中心圖書資料使用規則 (一) <br />
-        閱覽期刊、報紙或書籍，不提供外借服務，請在閱覽室閱讀，並請一次取一件，閱畢應歸還至指定位置或放回原位；且不得有影印、翻拍等行為。<br />
-        (二)來館民眾對本中心圖書資料應善盡保管維護之責，不得毀損、加註。<br />
+        第九點 <br/>
+        中心圖書資料使用規則 (一) <br/>
+        閱覽期刊、報紙或書籍，不提供外借服務，請在閱覽室閱讀，並請一次取一件，閱畢應歸還至指定位置或放回原位；且不得有影印、翻拍等行為。<br/>
+        (二)來館民眾對本中心圖書資料應善盡保管維護之責，不得毀損、加註。<br/>
       </p>
       <p>
         <img
-          class="icon_check"
-          src="../../../assets/about/booking/Icon open-circle-check.png"
+            class="icon_check"
+            src="../../../assets/about/booking/Icon open-circle-check.png"
         />
-        第十點<br />
-        本館中心提供導覽服務，前項服務以事先預約為原則。團體參觀者，應事先來函或電話預約；未經申請之機關或團體，如於本中心內進行活動者，本中心得予以制止，經規勸不聽者，本中心得視行為情節，必要時報請警察機關協助處理。<br />
+        第十點<br/>
+        本館中心提供導覽服務，前項服務以事先預約為原則。團體參觀者，應事先來函或電話預約；未經申請之機關或團體，如於本中心內進行活動者，本中心得予以制止，經規勸不聽者，本中心得視行為情節，必要時報請警察機關協助處理。<br/>
       </p>
       <p>
         <img
-          class="icon_check"
-          src="../../../assets/about/booking/Icon open-circle-check.png"
+            class="icon_check"
+            src="../../../assets/about/booking/Icon open-circle-check.png"
         />
-        第十一點 <br />
-        辦公區域(為未開放區)未經許可不得擅入及擅自拿取辦公室(含志工值班櫃檯)物品。<br />
+        第十一點 <br/>
+        辦公區域(為未開放區)未經許可不得擅入及擅自拿取辦公室(含志工值班櫃檯)物品。<br/>
       </p>
       <p>
         <img
-          class="icon_check"
-          src="../../../assets/about/booking/Icon open-circle-check.png"
+            class="icon_check"
+            src="../../../assets/about/booking/Icon open-circle-check.png"
         />
-        第十二點 <br />
-        賠償責任 本中心相關設施及設備使用，來館者應善盡維護之責。<br />
-        偷竊、污損各類型館藏圖書資料者（含展覽場），責令按所偷竊、污損之館藏圖書資料價格二倍賠償。<br />
-        毀損本中心所屬設施、設備者，由本中心代為修護，惟相關費用須由毀損者負擔，若不能修復，毀損者應照價賠償。<br />
-        違反前述規定及本要點之第七點、第八點、第九點及第十一點規定者，經規勸不聽，本中心得視行為情節，必要時報請警察機關究辦犯罪行為。<br />
+        第十二點 <br/>
+        賠償責任 本中心相關設施及設備使用，來館者應善盡維護之責。<br/>
+        偷竊、污損各類型館藏圖書資料者（含展覽場），責令按所偷竊、污損之館藏圖書資料價格二倍賠償。<br/>
+        毀損本中心所屬設施、設備者，由本中心代為修護，惟相關費用須由毀損者負擔，若不能修復，毀損者應照價賠償。<br/>
+        違反前述規定及本要點之第七點、第八點、第九點及第十一點規定者，經規勸不聽，本中心得視行為情節，必要時報請警察機關究辦犯罪行為。<br/>
       </p>
       <p>
         <img
-          class="icon_check"
-          src="../../../assets/about/booking/Icon open-circle-check.png"
+            class="icon_check"
+            src="../../../assets/about/booking/Icon open-circle-check.png"
         />
-        第十三點 <br />
-        其他<br />
-        (一)為維護公共安全，本中心得安裝網路監控與管理軟體，並架設監視系統。<br />
-        (二)遇緊急事件時，請依服務人員之指示避難或疏散。<br />
+        第十三點 <br/>
+        其他<br/>
+        (一)為維護公共安全，本中心得安裝網路監控與管理軟體，並架設監視系統。<br/>
+        (二)遇緊急事件時，請依服務人員之指示避難或疏散。<br/>
       </p>
       <p>
         <img
-          class="icon_check"
-          src="../../../assets/about/booking/Icon open-circle-check.png"
+            class="icon_check"
+            src="../../../assets/about/booking/Icon open-circle-check.png"
         />
-        第十四點<br />
-        本規則依行政程序報請社會局核備後公布實施，修正時亦同。<br />
+        第十四點<br/>
+        本規則依行政程序報請社會局核備後公布實施，修正時亦同。<br/>
       </p>
+    </div>
 
-      <p class="download_button">
-        <a
-          href="https://docs.google.com/document/d/1p2SM6-HGWeJknO9FiBsbJ9JFORT8BNAF/edit?usp=sharing&ouid=115132396574802911669&rtpof=true&sd=true"
+    <div class="book_action_container">
+      <div>
+        <p class="download_button">
+          <a
+              href="https://docs.google.com/document/d/1p2SM6-HGWeJknO9FiBsbJ9JFORT8BNAF/edit?usp=sharing&ouid=115132396574802911669&rtpof=true&sd=true"
           ><img
-            class="download"
-            src="../../../assets/about/booking/button_download.png"
-        /></a>
-      </p>
+              class="download"
+              src="../../../assets/about/booking/button_download.png"
+          /></a>
+        </p>
+      </div>
+      <div>
+        <iframe
+            src="https://calendar.google.com/calendar/embed?src=3ic4ib1rv215fp5ja9f20u2ogk%40group.calendar.google.com&ctz=Asia%2FTaipei"
+            style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+      </div>
     </div>
-    <div class="img_container">
-      <img class="place_img" src="../../../assets/about/booking/place1.jpg" />
-      <img class="place_img" src="../../../assets/about/booking/place2.jpg" />
-    </div>
+
   </div>
 </template>
 
@@ -204,7 +216,6 @@ export default {};
   border: 8px solid #f6a096;
   border-radius: 40px;
   padding: 40px;
-  display: flex;
 }
 
 .img_container {
@@ -217,6 +228,7 @@ export default {};
   width: 300px;
   height: 200px;
   object-fit: cover;
+  margin-bottom: 10px;
 }
 
 .content_container {
@@ -270,5 +282,11 @@ h1 {
 
 .breadcrumb_list > li:first-child::before {
   display: none;
+}
+
+.book_action_container {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
 </style>
